1a2b0d6e843550f5b3e735518397eb6ee6ce4b02
idolago <idolago94@gmail.com>
2020-03-12 10:34:47 +0200

    post multiple reports

--- routes/reports.js
+++ routes/reports.js
@@ -4,18 +4,22 @@ const Report = require('../models/Report');
 
 router.get('/all', async(req, res, next) => {
   console.log('GET /reports/all -> get all reports');
-  let allReports = await Report.find();
+  let allReports = await Report.find;
   res.json(allReports);
 });
 
 router.post('/', async(req, res, next) => {
     console.log('POST /reports -> post new report');
     let body = req.body.report;
-    let newReport = new Report({
-        ...body,
-        reportDate: new Date()
-    });
-    let response = await newReport.save();
+    body.map(report => report.reportDate = new Date());
+    // let response = await newReport.save();
+    Report.insertMany(body, (err, docs) => {
+      if(err) {
+        console.log(err);
+      } else {
+        res.json(docs);
+      }
+    })
     res.json(response);
 })
 
