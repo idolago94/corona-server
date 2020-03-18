f6dc7f4e7f037c67be921a0946e625e18eecf81b
idolago <idolago94@gmail.com>
2020-03-18 17:06:01 +0200

    schedule push notification

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var schedule = require('node-schedule');
+const fetch = require('node-fetch');
 
 const reportRouter = require('./routes/reports');
 const verifyRouter = require('./routes/verify');
@@ -12,6 +14,40 @@ const verifyRouter = require('./routes/verify');
 var app = express();
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/corona`);
+
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    │
+// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+// │    │    │    │    └───── month (1 - 12)
+// │    │    │    └────────── day of month (1 - 31)
+// │    │    └─────────────── hour (0 - 23)
+// │    └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+
+var sendNotification = schedule.scheduleJob(process.env.PUSH_SCHEDULE, function(){
+  console.log('sendNotification');
+  let bodyRequest = {
+    "request": {
+      "application": "4E22D-DBF89", // Pushwoosh application code
+      "auth": "q0d5z9n6GwgI3XaUdwhpxM2poyk700pq1Nxw0DfGMCCLbMHX7dxgadKRDjNDjhUid7RcY45RTBCqClYHH5w1", // API access token from Pushwoosh Control Panel
+      "notifications": [
+        {
+          // Content settings 
+          "send_date": "now",  // required. YYYY-MM-DD HH:mm OR 'now'
+          "ignore_user_timezone": true, // or false, required
+          "content": {  // required, object( language1: 'content1', language2: 'content2' ) OR string. Ignored for Windows 8, use "wns_content" instead. (Use \n for multiline text. Ex: "hello\nfriend")
+            "en": process.env.PUSH_MESSAGE
+          },
+        }
+      ]
+    }
+  }
+  fetch(`https://cp.pushwoosh.com/json/1.3/createMessage`,{
+    method: 'POST',
+    body: JSON.stringify(bodyRequest)
+  }).then(res => res.json()).then(response => console.log('Sendd notification response: ', response));
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
